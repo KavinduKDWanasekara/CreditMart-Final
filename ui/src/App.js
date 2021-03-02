d154564fd2ad7cb2ae43aeaf280e39150310160c
Abdul Baasith <61641051+baasithshiyam@users.noreply.github.com>
2021-03-02 12:06:51 +0530

    About us commit
    https://www.smashingmagazine.com/2020/02/tailwindcss-react-project/
    
    refer this for idea
    https://tailwindcss.com/docs/guides/create-react-app

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,14 +1,15 @@
 // import './App.css';
 // import WelcomePage from './pages/WelcomePage';
 import ContractPage from './pages/ContractPage';
-
+import AboutUs from './pages/AboutUs';
 
 function App() {
   return (
     <div className="App">
       {/* <h1>HEllo</h1> */}
       {/* <WelcomePage/> */}
-      <ContractPage/>
+      {/* <ContractPage/> */}
+< AboutUs />
 
     </div>
   );
