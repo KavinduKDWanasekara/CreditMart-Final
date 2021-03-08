7adea8765871eaf57d625e31deffad78504fce88
SafrazNazar <safraz.20191066@iit.ac.lk>
2021-03-08 22:10:18 +0530

    initiated simple jwt

--- backend/views.py
+++ backend/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User, auth
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+
+@csrf_exempt
+def register(request):
+    if request.method == 'POST':
+        username = request.POST('username')
+        email = request.POST('email')
+        password = request.POST('password')
+
+        user = User.objects.create_user(username=username, email=email, password=password)
+        user.save()
+        print('User Created!!')
+    else:
+        return render(request, 'build/index.html')
