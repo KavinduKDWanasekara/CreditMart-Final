c47a87eeba48290d3d09618ad543f520f003ab6c
Navindu <navindusenuja59@gmail.com>
2021-03-10 23:19:05 +0530

    Register JS with CSS

--- ui/src/components/auth/Register.js
+++ ui/src/components/auth/Register.js
@@ -69,9 +69,35 @@ class Register extends Component {
     render() {
         const { data, errors } = this.state;
         return (
+            <section className="relative w-full h-full py-40 min-h-screen">
+ <div className="absolute top-0 w-full h-full bg-gray-900 bg-no-repeat bg-full"
+    style={{ 
+  backgroundImage: `url(${process.env.PUBLIC_URL + 'static/images/background1.jpg'})`,
+  // backgroundRepeat: 'no-repeat',
+  // // position:"fixed",
+  // width:'100%' 
+  
+}}>
+
+<div className="container mx-auto px- h-full">
+        <div className="flex content-center items-center justify-center h-full">
+          <div className="w-full lg:w-6/12 px-4">
+            <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-gray-300 border-0">
+              <div className="rounded-t mb-0 px-6 py-6">
+
+              <div className="text-center mb-3">
+                    <h3 className="text-gray-600 text-3xl font-bold tracking-widest ">  Sign Up
+                  </h3>
+                  </div>
+
             <Form onSubmit={this.handleSubmit} className="w-72">
                 <FormGroup>
-                    <Label for="userName">First Name</Label>
+                <label
+                      for="userName"
+                      className="text-gray-600 text-md tracking-wide"
+                    >
+                      Username
+              </label>
                     <Input id="userName" value={data.userName} invalid={errors.userName ? true : false} name="userName" onChange={this.handleChange} />
                     <FormFeedback>{errors.userName}</FormFeedback>
                 </FormGroup>
@@ -79,8 +105,8 @@ class Register extends Component {
                
 
                 <FormGroup>
-                    <Label for="email">Email</Label>
-                    <Input id="email" value={data.email} invalid={errors.email ? true : false} name="email" onChange={this.handleChange} />
+                    <Label for="email" className="text-gray-600 text-md tracking-wide ">Email</Label>
+                    <Input id="email" value={data.email} invalid={errors.email ? true : false} name="email" onChange={this.handleChange}  />
                     <FormFeedback>{errors.email}</FormFeedback>
                 </FormGroup>
 
@@ -92,12 +118,23 @@ class Register extends Component {
 
                 <FormGroup>
                     <Label for="confirmPassword">Confirm Password</Label>
-                    <Input id="confirmPassword" value={data.confirmPassword} type="password" name="confirmPassword" invalid={errors.confirmPassword ? true : false} onChange={this.handleChange} />
+                    <Input id="confirmPassword" value={data.confirmPassword} type="password" name="confirmPassword" invalid={errors.confirmPassword ? true : false} onChange={this.handleChange}
+                     />
                     <FormFeedback>{errors.confirmPassword}</FormFeedback>
                 </FormGroup>
+                <div className="text-center ">
+                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded" >Register</button>
+                </div>
 
-                <Button color="primary">Register</Button>
             </Form>
+            </div>
+            </div>
+            </div>
+          </div>
+        </div>
+      
+            </div>
+            </section>
         );
     }
 }
