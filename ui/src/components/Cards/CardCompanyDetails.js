ff3fc4d833045949fd296b3ad2192547e680eec1
AshfaaqAhamed <ashfaaqahamed17@gmail.com>
2021-03-16 12:30:58 +0530

    added form group & formfeedback

--- ui/src/components/Cards/CardCompanyDetails.js
+++ ui/src/components/Cards/CardCompanyDetails.js
@@ -1,6 +1,6 @@
 import { data } from 'autoprefixer';
 import React, { Component } from 'react'
-import { Form } from 'reactstrap'
+import { Form, FormGroup, FormFeedback } from 'reactstrap'
 import axiosInstance from '../../axios';
 
 
@@ -49,6 +49,7 @@ export class CardCompanyDetails extends Component {
     }
 
     handleSubmit = (e) => {
+        
         e.preventDefault();
      
 
@@ -67,7 +68,6 @@ export class CardCompanyDetails extends Component {
                 description: data.description
 			})
 			.then((res) => {
-				// this.props.history.push('/login');
 				console.log(res);
 				console.log(res.data);
 			});
@@ -88,6 +88,7 @@ export class CardCompanyDetails extends Component {
                     <div className=" mb-2 flex justify-center">
                         <table className=" text-center">
                             <tr>
+                            <FormGroup>
                                 <div className="md:w-full px-1/2 text-left md:mb-0">
                                 <td>
                                     <label className="uppercase tracking-wide text-black text-xs font-bold mb-2">
@@ -96,13 +97,16 @@ export class CardCompanyDetails extends Component {
                                 </td>
                                 <td>
                                     <div>
-                                        <input name="company_name" value={data.company_name}  onChange={this.changeHandler} invalid={errors.company_name ? true : false} className="w-96 bg-gray-200 text-black border border-gray-200 rounded py-3 mx-4 px-4 mb-3" id="addInfoData" type="text" placeholder="20XX"/>
+                                        <input name="company_name" value={data.company_name}  onChange={this.changeHandler} invalid={errors.company_name ? true : false} className="w-96 bg-gray-200 text-black border border-gray-200 rounded py-3 mx-4 px-4 mb-3" id="addInfoData" type="text" placeholder="Enter Company Name"/>
                                     </div>
                                 </td>
                                 </div>
+                                <FormFeedback>{errors.company_name}</FormFeedback>
+                            </FormGroup>
                             </tr>
 
                             <tr>
+                            <FormGroup>
                             <div className="md:w-full px-1/2 text-left md:mb-0">
                                 <td>
                                     <label className="uppercase tracking-wide text-black text-xs font-bold mb-2">
@@ -111,13 +115,17 @@ export class CardCompanyDetails extends Component {
                                 </td>
                                 <td>
                                     <div>
-                                        <input name="contact_number" value={data.contact_number}  onChange={this.changeHandler} invalid={errors.contact_number ? true : false} className="w-96 bg-gray-200 text-black border border-gray-200 rounded py-3 mx-4 px-4 mb-3" id="addInfoData" type="text" placeholder="XXXXX.XX"/>
+                                        <input name="contact_number" value={data.contact_number}  onChange={this.changeHandler} invalid={errors.contact_number ? true : false} className="w-96 bg-gray-200 text-black border border-gray-200 rounded py-3 mx-4 px-4 mb-3" id="addInfoData" type="text" placeholder="Enter Contact Number"/>
                                     </div>
                                 </td>
                             </div>
+                            <FormFeedback>{errors.contact_number}</FormFeedback>
+
+                            </FormGroup>
                             </tr>
 
                             <tr>
+                            <FormGroup>
                             <div className="md:w-full px-1/2 text-left md:mb-0">
                                 <td>
                                     <label className="uppercase tracking-wide text-black text-xs font-bold mb-2">
@@ -126,13 +134,17 @@ export class CardCompanyDetails extends Component {
                                 </td>
                                 <td>
                                     <div>
-                                        <input name="location" value={data.location} onChange={this.changeHandler} invalid={errors.location ? true : false} className="w-96 bg-gray-200 text-black border border-gray-200 rounded py-3 mx-4 px-4 mb-3" id="addInfoData" type="text" placeholder="XXXXX.XX"/>
+                                        <input name="location" value={data.location} onChange={this.changeHandler} invalid={errors.location ? true : false} className="w-96 bg-gray-200 text-black border border-gray-200 rounded py-3 mx-4 px-4 mb-3" id="addInfoData" type="text" placeholder="Enter Location"/>
                                     </div>
                                 </td>
                             </div>
+                            <FormFeedback>{errors.location}</FormFeedback>
+
+                            </FormGroup>
                             </tr>
                         
                             <tr>
+                                <FormGroup>
                             <div className="md:w-full px-1/2 text-left md:mb-0">
                                 <td>
                                     <label className="uppercase tracking-wide text-black text-xs font-bold mb-2">
@@ -141,13 +153,17 @@ export class CardCompanyDetails extends Component {
                                 </td>
                                 <td>
                                     <div>
-                                        <input name="business_type" value={data.business_type} onChange={this.changeHandler} invalid={errors.business_type ? true : false} className="w-96 bg-gray-200 text-black border border-gray-200 rounded py-3 mx-4 px-4 mb-3" id="addInfoData" type="text" placeholder="XXXXX.XX"/>
+                                        <input name="business_type" value={data.business_type} onChange={this.changeHandler} invalid={errors.business_type ? true : false} className="w-96 bg-gray-200 text-black border border-gray-200 rounded py-3 mx-4 px-4 mb-3" id="addInfoData" type="text" placeholder="Enter Business Type"/>
                                     </div>
                                 </td>
                             </div>
+                            <FormFeedback>{errors.business_type}</FormFeedback>
+
+                            </FormGroup>
                             </tr>
 
                             <tr>
+                                <FormGroup>
                             <div className="md:w-full px-1/2 text-left md:mb-0">
                                 <td>
                                     <label className="uppercase tracking-wide text-black text-xs font-bold mb-2">
@@ -156,10 +172,12 @@ export class CardCompanyDetails extends Component {
                                 </td>
                                 <td>
                                     <div>
-                                        <input name="description" value={data.description} onChange={this.changeHandler} invalid={errors.description ? true : false} className="w-96 bg-gray-200 text-black border border-gray-200 rounded py-3 mx-4 px-4 mb-3" id="addInfoData" type="text" placeholder="XXXXX.XX"/>
+                                        <input name="description" value={data.description} onChange={this.changeHandler} invalid={errors.description ? true : false} className="w-96 bg-gray-200 text-black border border-gray-200 rounded py-3 mx-4 px-4 mb-3" id="addInfoData" type="text" placeholder="Enter Company Description"/>
                                     </div>
                                 </td>
                             </div>
+                            <FormFeedback>{errors.description}</FormFeedback>
+                            </FormGroup>
                             </tr>
                         </table>
                     </div>
