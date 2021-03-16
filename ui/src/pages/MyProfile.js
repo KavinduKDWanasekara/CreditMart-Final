c99b5f4fd5fe286ea74c0807ebaf7eeae7f47436
Navindu <navindusenuja59@gmail.com>
2021-03-16 15:10:19 +0530

    Add button to Financial data input

--- ui/src/pages/MyProfile.js
+++ ui/src/pages/MyProfile.js
@@ -2,6 +2,8 @@ import React from "react";
 import Navbar from "../components/Navbar"
 import ProfileAddInfo from "../components/Cards/ProfileAddInfo";
 import CardCompanyDetails from "../components/Cards/CardCompanyDetails";
+import { MDBBtn,  } from "mdbreact";
+import { Link } from "react-router-dom";
 
 export default function MyProfile() {
   return (
@@ -25,12 +27,7 @@ export default function MyProfile() {
                   </div>
                   <div className="w-full lg:w-4/12 px-4 lg:order-3 lg:text-right lg:self-center ">
                     <div className="py-6 px-3 mt-32 sm:mt-0">
-                      <button
-                        className="bg-blue-500 active:bg-blue-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1 ease-linear transition-all duration-150"
-                        type="button"
-                        >
-                        Page Develouper
-                      </button>
+                    <Link  to="/profileaddinfo"><MDBBtn color="blue" className="rounded-lg p-2 w-24 h-12">Enter Financial Data</MDBBtn></Link>
                     </div>
                   </div>
                   <div className="w-full lg:w-4/12 px-4 lg:order-1">
