36bc9cb4ba7ab5ea36e7286da0b34eac60c8308b
AshfaaqAhamed <ashfaaqahamed17@gmail.com>
2021-03-18 10:27:15 +0530

    Changed some cards in the dashboard

--- ui/src/components/Cards/CardBarChart.js
+++ ui/src/components/Cards/CardBarChart.js
@@ -4,18 +4,19 @@ import Chart from "chart.js";
 export default function CardBarChart() {
   React.useEffect(() => {
     fetch(
-      'https://jsonplaceholder.typicode.com/posts',
+      'http://127.0.0.1:8000/api/pd',
       {
         method: "GET",
         headers: new Headers({
-          Accept: "application/vnd.github.vloak-preview"
+          Authorization : 'Token '+localStorage.getItem('token'), 
+          Accept: "application/json"
         })
       })
     .then(res => res.json())
     .then(response => {
       // setCommitHistory(response.items);
       // setIsLoading(false);
-      console.log(response[6]);
+      console.log(response);
     })
     .catch(console.error());
   
@@ -23,31 +24,23 @@ export default function CardBarChart() {
       type: "bar",
       data: {
         labels: [
-          "January",
-          "February",
-          "March",
-          "April",
-          "May",
-          "June",
-          "July",
+          "2015",
+          "2016",
+          "2017",
+          "2018",
+          "2019",
+          "2020",
+          "2021",
         ],
         datasets: [
           {
-            label: new Date().getFullYear(),
+            label: "Sales",
             backgroundColor: "#ed64a6",
             borderColor: "#ed64a6",
             data: [30, 78, 56, 34, 100, 45, 13],
             fill: false,
             barThickness: 8,
-          },
-          {
-            label: new Date().getFullYear() - 1,
-            fill: false,
-            backgroundColor: "#4c51bf",
-            borderColor: "#4c51bf",
-            data: [27, 68, 86, 74, 10, 4, 87],
-            barThickness: 8,
-          },
+          }
         ],
       },
       options: {
@@ -75,37 +68,38 @@ export default function CardBarChart() {
         scales: {
           xAxes: [
             {
-              display: false,
+              display: true,
               scaleLabel: {
-                display: true,
-                labelString: "Month",
+                // display: true,
+                // labelString: "Month",
               },
-              gridLines: {
-                borderDash: [2],
-                borderDashOffset: [2],
-                color: "rgba(33, 37, 41, 0.3)",
-                zeroLineColor: "rgba(33, 37, 41, 0.3)",
-                zeroLineBorderDash: [2],
-                zeroLineBorderDashOffset: [2],
-              },
+              // gridLines: {
+              //   drawBorder: false,
+              //   borderDash: [0],
+              //   borderDashOffset: [0],
+              //   color: "rgba(33, 37, 41, 0.3)",
+              //   zeroLineColor: "rgba(33, 37, 41, 0.3)",
+              //   zeroLineBorderDash: [2],
+              //   zeroLineBorderDashOffset: [2],
+              // },
             },
           ],
           yAxes: [
             {
               display: true,
               scaleLabel: {
-                display: false,
-                labelString: "Value",
+                // display: false,
+                // labelString: "Value",
               },
-              gridLines: {
-                borderDash: [2],
-                drawBorder: false,
-                borderDashOffset: [2],
-                color: "rgba(33, 37, 41, 0.2)",
-                zeroLineColor: "rgba(33, 37, 41, 0.15)",
-                zeroLineBorderDash: [2],
-                zeroLineBorderDashOffset: [2],
-              },
+              // gridLines: {
+              //   borderDash: [2],
+              //   drawBorder: false,
+              //   borderDashOffset: [2],
+              //   color: "rgba(33, 37, 41, 0.2)",
+              //   zeroLineColor: "rgba(33, 37, 41, 0.15)",
+              //   zeroLineBorderDash: [2],
+              //   zeroLineBorderDashOffset: [2],
+              // },
             },
           ],
         },
@@ -125,8 +119,8 @@ export default function CardBarChart() {
               <h6 className="uppercase text-gray-500 mb-1 text-xs font-semibold">
                 Performance
               </h6>
-              <h2 className="text-gray-800 text-xl font-semibold">
-                Total orders
+              <h2 className=" text-xl font-semibold">
+                Credit Sales
               </h2>
             </div>
           </div>
