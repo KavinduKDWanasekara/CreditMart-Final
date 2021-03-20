4bc2862e6ea782b0f0ec18fd20cb0ea5b579d23a
kavinduwanasekara <kavindu.20191067@iit.ac.lk>
2021-03-20 20:06:01 +0530

    Added some tailwind css

--- ui/src/pages/MyProfileNew.js
+++ ui/src/pages/MyProfileNew.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react'
 
 export class MyProfileNew extends Component {
+
+    onChangeHandler=event=>{
+
+        console.log(event.target.files[0])
+    
+    }
+
     render() {
         return (
             <>
@@ -19,75 +26,43 @@ export class MyProfileNew extends Component {
               className="w-full h-full absolute opacity-50 bg-black"
             ></span>
           </div>
-          <div
-            className="top-auto bottom-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden h-70-px"
-            style={{ transform: "translateZ(0)" }}
-          >
-            <svg
-              className="absolute bottom-0 overflow-hidden"
-              xmlns="http://www.w3.org/2000/svg"
-              preserveAspectRatio="none"
-              version="1.1"
-              viewBox="0 0 2560 100"
-              x="0"
-              y="0"
-            >
-              <polygon
-                className="text-gray-300 fill-current"
-                points="2560 0 2560 100 0 100"
-              ></polygon>
-            </svg>
-          </div>
         </section>
         <section className="relative py-44 bg-gray-300">
           <div className="container mx-9 px-10">
-            <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-10 shadow-xl rounded-lg -mt-64">
+            <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-10 shadow-xl rounded-lg -mt-56">
+            
               <div className="px-6">
-                <div className="flex flex-wrap justify-center">
-                  <div className="w-full lg:w-3/12 px-4 lg:order-3 flex justify-center">
-                    <div className="relative">
-                      <img
-                        alt="..."
-                        src={("https://st.depositphotos.com/2101611/3925/v/600/depositphotos_39258143-stock-illustration-businessman-avatar-profile-picture.jpg")}
-                        className="shadow-xl rounded-full h-auto align-middle border-none absolute -m-16 -ml-20 lg:-ml-16 max-w-150-px"
-                      />
-                    </div>
-                  </div>
-                  <div className="w-full lg:w-4/12 px-4 lg:order-3 lg:text-right lg:self-center">
-                    <div className="py-6 px-3 mt-32 sm:mt-0">
-                      {/* <button
-                        className="bg-blue-500 active:bg-blue-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1 ease-linear transition-all duration-150"
-                        type="button"
-                      >
-                        Connect
-                      </button> */}
-                    </div>
-                  </div>
-                  <div className="w-full lg:w-4/12 px-4 lg:order-1">
-                    <div className="flex justify-center py-4 lg:pt-4 pt-8">
-                      <div className="mr-4 p-3 text-center">
-                        {/* <span className="text-xl font-bold block uppercase tracking-wide text-gray-700">
-                          22
-                        </span> */}
-                        <button className="bg-blue-500 active:bg-blue-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1 ease-linear transition-all duration-150"
-                        type="button">Edit Profile</button>
-                      </div>
-                      <div className="mr-4 p-3 text-center">
-                        {/* <span className="text-xl font-bold block uppercase tracking-wide text-gray-700">
-                          10
-                        </span> */}
-                        <button className="bg-blue-500 active:bg-blue-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1 ease-linear transition-all duration-150"
-                        type="button">Edit Financial Data</button>
-                      </div>
-                      <div className="lg:mr-4 p-3 text-center">
-                        {/* <span className="text-xl font-bold block uppercase tracking-wide text-gray-700">
-                          89
-                        </span>
-                        <span className="text-sm text-gray-500">Comments</span> */}
-                      </div>
-                    </div>
-                  </div>
+              <div className="py-6 px-3 mt-32 sm:mt-0">
+                        {/* <button className="bg-blue-500 active:bg-blue-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1 ease-linear transition-all duration-150"
+                        type="button">Edit Profile</button> */}
+                        <button class="h-10 px-5 text-coolGray-700 transition-colors duration-150 border border-coolGray-500 rounded-lg focus:shadow-outline hover:bg-gray-300 hover:text-coolGray-100">Edit Profile</button>
+                        {/* <button className="bg-blue-500 active:bg-blue-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1 ease-linear transition-all duration-150"
+                        type="button">Edit Financial Data</button> */}
+                        <button class="h-10 px-5 text-coolGray-700 transition-colors duration-150 border border-coolGray-500 rounded-lg focus:shadow-outline hover:bg-gray-300 hover:text-coolGray-100">Add Financial Data</button>
+                        
                 </div>
+                      
+              <div className="flex flex-wrap justify-center -mt-36">
+                  <img
+                        alt="..."
+                        // src={("https://st.depositphotos.com/2101611/3925/v/600/depositphotos_39258143-stock-illustration-businessman-avatar-profile-picture.jpg")}
+                        src={("https://scontent.fcmb2-1.fna.fbcdn.net/v/t1.0-9/16998754_217016665439934_8147080226351143645_n.jpg?_nc_cat=103&ccb=1-3&_nc_sid=174925&_nc_ohc=82cUc6mLSroAX-WpPjx&_nc_ht=scontent.fcmb2-1.fna&oh=06a02ec30ac75a1e23912957e669b3d6&oe=607D52A9")}
+                        className="inline object-cover w-44 h-44 -mr- 6 rounded-full"
+                      />
+                      
+                      <button  class="inline-flex items-center justify-center w-10 h-10 mr-2 text-gray-700 transition-colors duration-150 bg-white rounded-full focus:shadow-outline hover:bg-gray-200" >
+                          
+                            <svg class="w-4 h-4 fill-current" viewBox="0 0 20 20"><path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z"></path> </svg>
+                            
+                            
+                      </button>
+                      
+
+                        
+                      
+                </div>
+                      
+                
                 <div className="text-center mt-12">
                   <h3 className="text-4xl font-semibold leading-normal mb-2 text-gray-800 mb-2">
                     Jenna Stones
@@ -130,13 +105,6 @@ export class MyProfileNew extends Component {
                         warm, intimate feel with a solid groove structure. An
                         artist of considerable range.
                       </p>
-                      {/* <a
-                        href="#pablo"
-                        className="font-normal text-blue-500"
-                        onClick={(e) => e.preventDefault()}
-                      >
-                        Show more
-                      </a> */}
                     </div>
                   </div>
                 </div>
