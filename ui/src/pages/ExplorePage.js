24f037e993e6311725917c15a01b6655ba00380b
Abdul Baasith <61641051+baasithshiyam@users.noreply.github.com>
2021-03-21 22:29:17 +0530

    Explore page

--- ui/src/pages/ExplorePage.js
+++ ui/src/pages/ExplorePage.js
@@ -18,6 +18,7 @@ class ExplorePage extends Component{
             search: ""
         },
         errors: {},
+        
     });
 
     changeHandler = (e) => {
@@ -59,6 +60,7 @@ class ExplorePage extends Component{
 			.then((res) => {
 				console.log(res);
 				console.log(res.data);
+                
 			});
             // Resetting the form
             this.setState(this.getInitialState());
@@ -102,14 +104,7 @@ render(){
 
             <div className="container my-12 mx-auto px-4 md:px-12">
                 <div className="flex flex-wrap -mx-1 lg:-mx-4">
-                <CardExplorePage/>
-                <CardExplorePage/>
-                <CardExplorePage/>
-                <CardExplorePage/>
-                <CardExplorePage/>
-                <CardExplorePage/>
-                <CardExplorePage/>
-
+                
                 </div>
         </div>
         <Footer/>
